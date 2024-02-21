3a0d9fcf3f16d7ee0e9c883e73c9413ecbfb8af3
Chino <carlos.aguirre.alcala@hotmail.com>
2024-02-21 13:12:40 -0600

    resa

--- Control Negocios/Formularios/18 Restaurantes/frmPasa_Mesa.Designer.vb
+++ Control Negocios/Formularios/18 Restaurantes/frmPasa_Mesa.Designer.vb
@@ -52,7 +52,7 @@ Partial Class frmPasa_Mesa
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.BackColor = System.Drawing.Color.White
+        Me.BackColor = System.Drawing.Color.Moccasin
         Me.ClientSize = New System.Drawing.Size(207, 105)
         Me.Controls.Add(Me.txtclave)
         Me.Controls.Add(Me.Label1)
