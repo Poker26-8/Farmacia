7751719456a9a08b9f81704b09d53b00eb50d765
alecks2539@gmail.com <alecks2539@gmail.com>
2024-04-26 10:11:48 -0600

    jj

--- Control Negocios/Formularios/0 Inicio/Inicio.vb
+++ Control Negocios/Formularios/0 Inicio/Inicio.vb
@@ -1417,6 +1417,23 @@ Public Class Inicio
             cmd1.ExecuteNonQuery()
             cnn1.Close()
         End Try
+        Try
+            cnn1.Close()
+            cnn1.Open()
+            cmd1 = cnn1.CreateCommand
+            cmd1.CommandText = "SELECT Referencia FROM Clientes"
+            rd1 = cmd1.ExecuteReader
+            If rd1.Read Then
+            End If
+            rd1.Close()
+            cnn1.Close()
+        Catch ex As Exception
+            rd1.Close()
+            cmd1 = cnn1.CreateCommand
+            cmd1.CommandText = "ALTER TABLE Clientes add column Referencia varchar(255) default ''"
+            cmd1.ExecuteNonQuery()
+            cnn1.Close()
+        End Try
 
         'ventas
         Try
