87601037a1514e990bcb98b1e91a1f861a54385b
alecks2539@gmail.com <alecks2539@gmail.com>
2024-06-17 09:57:17 -0600

    xxxx

--- Control Negocios/Formularios/02 Activación de módulos/frmModulos.vb
+++ Control Negocios/Formularios/02 Activación de módulos/frmModulos.vb
@@ -132,4 +132,8 @@
         frmAct_Optica.BringToFront()
         Me.Close()
     End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
 End Class
