bb4be217852d5ae3d9b2a157dfcf06ea69798fc8
CARLOS AGUIRRE <carlos.aguirre.alcala@hotmail.com>
2024-08-29 17:56:32 -0600

    as

--- Control Negocios/Formularios/0 Inicio/frmLoad.vb
+++ Control Negocios/Formularios/0 Inicio/frmLoad.vb
@@ -409,6 +409,25 @@ Public Class frmLoad
 
     Public Sub verif()
 
+        'devoluciones
+        Try
+            cnn1.Close()
+            cnn1.Open()
+            cmd1 = cnn1.CreateCommand
+            cmd1.CommandText = "SELECT Comentario FROM dovoluciones"
+            rd1 = cmd1.ExecuteReader
+            If rd1.Read Then
+            End If
+            rd1.Close()
+            cnn1.Close()
+        Catch ex As Exception
+            rd1.Close()
+            cmd1 = cnn1.CreateCommand
+            cmd1.CommandText = "ALTER TABLE dovoluciones add column Comentario varchar(255) DEFAULT ''"
+            cmd1.ExecuteNonQuery()
+            cnn1.Close()
+        End Try
+
         'profesion
         Try
             cnn1.Close()
