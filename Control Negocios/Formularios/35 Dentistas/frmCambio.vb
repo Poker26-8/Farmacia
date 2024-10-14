1891dce1cf71351904c6efb577337cd580f1db9b
CARLOS AGUIRRE <carlos.aguirre.alcala@hotmail.com>
2024-10-14 09:36:19 -0600

    assaas

--- Control Negocios/Formularios/35 Dentistas/frmCambio.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class frmCambio
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Me.Close()
-    End Sub
-
-    Private Sub frmCambio_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-End Class
